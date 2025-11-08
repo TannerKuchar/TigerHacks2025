ca0a102025ed2b4f3dd274c01c2c66dfab36c73d
TannerKuchar <tannerkuchar@gmail.com>
2025-11-08 13:30:53 -0600

    Changed deployment of sever
    Must use "npm install" in default folder, and then go into globe-vite and run "npm install", then "npm run build". After use "cd .." and run "node server.js" and use the localhost:3000. Might need to add API_KEY to .env in the outer folder too. Not sure ngl.

--- globe-vite/main.js
+++ globe-vite/main.js
@@ -44,9 +44,9 @@ const detail = 12;
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshPhongMaterial({
-  map: loader.load("./textures/00_earthmap1k.jpg"),
-  specularMap: loader.load("./textures/02_earthspec1k.jpg"),
-  bumpMap: loader.load("./textures/01_earthbump1k.jpg"),
+  map: loader.load("/textures/00_earthmap1k.jpg"),
+  specularMap: loader.load("/textures/02_earthspec1k.jpg"),
+  bumpMap: loader.load("/textures/01_earthbump1k.jpg"),
   bumpScale: 0.04,
 });
 const earthMesh = new THREE.Mesh(geometry, material);
@@ -54,7 +54,7 @@ earthGroup.add(earthMesh);
 
 // Lights overlay
 const lightsMat = new THREE.MeshBasicMaterial({
-  map: loader.load("./textures/03_earthlights1k.jpg"),
+  map: loader.load("/textures/03_earthlights1k.jpg"),
   blending: THREE.AdditiveBlending,
 });
 const lightsMesh = new THREE.Mesh(geometry, lightsMat);
@@ -62,11 +62,11 @@ earthGroup.add(lightsMesh);
 
 // Clouds
 const cloudsMat = new THREE.MeshStandardMaterial({
-  map: loader.load("./textures/04_earthcloudmap.jpg"),
+  map: loader.load("/textures/04_earthcloudmap.jpg"),
   transparent: true,
   opacity: 0.8,
   blending: THREE.AdditiveBlending,
-  alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
+  alphaMap: loader.load('/textures/05_earthcloudmaptrans.jpg'),
 });
 const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
 cloudsMesh.scale.setScalar(1.003);
